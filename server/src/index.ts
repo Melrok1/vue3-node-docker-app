80ab920bdaa5e335049fee249a3f90fc3985cd4d
Martin Blaščák <martin.blascak86@gmail.com>
2025-07-14 08:29:28 +0200

    backend - added connection to mongoDB

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 const PORT = 3000
 
+mongoose.connect(process.env.MONGO_URI as string)
+  .then(() => console.log('✅ Connected to MongoDB Atlas'))
+  .catch(err => console.error('❌ MongoDB connection error:', err))
+
 app.get('/', (req, res) => {
-  res.send('Hello from Express + TypeScript!')
+  res.send('Hello from Express + TypeScript + MongoDB Atlas!')
 })
 
 app.listen(PORT, () => {
